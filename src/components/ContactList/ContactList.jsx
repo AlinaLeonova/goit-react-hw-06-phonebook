8824b7542615032543989aea4385e446d3edadf7
Alina Leonova <alinaa.leonova@gmail.com>
2020-12-14 20:42:09 +0200

    phonebook

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,41 @@
+import React from "react"
+import ContactItem from "../ContactItem"
+import { CSSTransition, TransitionGroup } from "react-transition-group"
+import "./ContactList.css"
+import { connect } from "react-redux"
+import contactsActions from "../../redux/contacts/contactsActions"
+
+const ContactList = ({ contacts, onRemoveContact }) => {
+  return (
+    <TransitionGroup component="ul" className="ContactList">
+      {contacts.map((item) => (
+        <CSSTransition
+          key={item.id}
+          timeout={250}
+          classNames="ContactList-item-slide"
+        >
+          <ContactItem
+            contact={item}
+            onRemoveContact={() => onRemoveContact(item.id)}
+          />
+        </CSSTransition>
+      ))}
+    </TransitionGroup>
+  )
+}
+
+
+
+const mapStateToProps = (state) => {
+  const { items, filter } = state.contacts
+  const filteredContacts = items.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return { contacts: filteredContacts }
+}
+
+const mapDispatchToProps = {
+  onRemoveContact: contactsActions.onRemoveContact,
+}
+export default connect(mapStateToProps, mapDispatchToProps)(ContactList)
